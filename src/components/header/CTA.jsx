20dc2ee829acc6e9818b7e1caa3358cc48cf4533
cimenakalo <140422952+cimenakalo@users.noreply.github.com>
2024-07-11 10:18:43 +0300

    master1

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,12 @@
+import import React from 'react'
+import CV from '../../assets/cv.pdf'
+const CTA = () => {
+  return (
+    <div className='cta'>
+      <a href="{cv}" download className='btn'>הורד קורות חיים</a>
+      <a href="#contact" className='btn btn-primary'>בוא נדבר</a>
+    </div>
+  )
+}
+
+export default CTA
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './header.css'
-
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 function Header() {
     return (
-      <div>Header</div>
+      <Header>
+       <div className="container header_container">
+          <h5> היי אני </h5>
+          <h1> שלמה </h1>
+          <h5 className="text-light">FullStack Developer</h5>
+          <CTA/>
+
+          <div className="me">
+            <img src="{ME}" alt="me" />
+          </div>
+
+          <a href="#contact" className='scroll_down'>Scroll Down</a>
+        </div>
+      </Header>
     )
   }
   
